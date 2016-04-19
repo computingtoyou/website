8a1f4e8c7f040b9641e2c49a5f9cb56f9b11ef40
marco <oliveira.marco.andre@gmail.com>
2016-04-19 00:09:02 -0300

    form de contato finalizado; logo ajustado para telas pequenas

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,67 +1,77 @@
-$( "#emailForm" ).submit(function( event ) {
-  debugger;
+$( "#send" ).on('click', function(event) {
+  // debugger;
   event.preventDefault();
 
   var dados = {
     nome : $('#nome').val(),
     email : $('#email').val(),
-    assunto : $('#assunto').val(),
-    mensagem : $('#mensagem').val()
+    _subject : $('#assunto').val(),
+    mensagem : $('#mensagem').val(),
+    _format : 'plain'
+    // _cc : 'tiemi@ufscar.br,lzaina@ufscar.br,lizier@ufscar.br'
   }
 
   if(validacao(dados)){
     $.ajax({
-      type: "POST",
-      url: "",
+      method: "POST",
+      url: "https://formspree.io/marco.oliveira@dcomp.sor.ufscar.br",
       data: dados,
+      dataType: "json",
       success: function(data){
-        Materialize.toast(data.data, 4000,'', function(){
+        Materialize.toast('Mensagem enviada!', 4000, '', function(){
           $('#emailForm')[0].reset();
         });
       },
       error: function(data){
-         Materialize.toast(data.data, 4000);
-      },
-      dataType: "json"
+         Materialize.toast('Desculpe-nos, ocorreu um erro :(', 4000);
+      }
     });
   }
 
 });
 
 var validacao = function(dados){
+  console.log(dados);
   var nomeValido = false;
   var emailValido = false;
   var assuntoValido = false;
   var mensagemValida = false;
 
   if(dados.nome == null || dados.nome == ""){
+    $('#nome').removeClass("valid");
     $('#nome').addClass("invalid");
   }else{
     $('#nome').addClass("valid");
+    $('#nome').removeClass("invalid");
     nomeValido = true;
   }
 
   if(dados.email == null || dados.email == "" || !isEmail(dados.email)){
+    $('#email').removeClass("valid");
     $('#email').addClass("invalid");
   }else{
     $('#email').addClass("valid");
+    $('#email').removeClass("invalid");
     emailValido = true;
   }
 
-  if(dados.assunto == null){
+  if(dados._subject == null){
+    $('#assunto').removeClass("valid");
     $('#assunto').addClass("invalid");
   }else{
     $('#assunto').addClass("valid");
+    $('#assunto').removeClass("invalid");
     assuntoValido = true;
   }
 
   if(dados.mensagem == null || dados.mensagem == ""){
+    $('#mensagem').removeClass("valid");
     $('#mensagem').addClass("invalid");
   }else{
     $('#mensagem').addClass("valid");
+    $('#mensagem').removeClass("invalid");
     mensagemValida = true;
   }
-
   return nomeValido && emailValido && assuntoValido && mensagemValida;
 };
 
